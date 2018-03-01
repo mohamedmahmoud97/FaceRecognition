6898f2b389dcb156d82219c9a6f86d7ef8f2b629
mohamedmahmoud97 <mohamedmorsy8438@gmail.com>
2018-03-01 22:30:18 +0200

    completed all pca implementation and tested the projection of the eighen faces.

--- classifiers/pca.py
+++ classifiers/pca.py
@@ -9,7 +9,47 @@ class PCA:
 	def train(self, X, alpha, load_path=None, verbose=False):
 		
 		if load_path is not None and os.path.isfile(load_path):
-			self._proj_mat = np.real(np.load(load_path))
+			#mean
+			mean = np.mean(X, axis=0)
+			mean = np.expand_dims(mean,axis=1)
+			
+			#the centered matrix
+			mean = np.mean(X,axis=0)
+			Z = X - mean
+
+			#covarinace matrix
+			cov = np.cov(X.T)
+			data = np.load('pca_projection.npz');EiVal = data['name1'];EiVec = data['name2']
+			tempEiVal = np.absolute(EiVal)
+			tempEiVec = np.absolute(EiVec)
+
+			print("shape: = : ", tempEiVec.shape)
+
+			maxEiIndex = []
+
+			#compute the acceptable explained variance to exceed the self.alpha
+			for x in range(len(tempEiVal)):
+				maxEiIndex.append(tempEiVal[x])
+				maxIndex = x
+				if (np.sum(maxEiIndex)/np.trace(cov))>alpha:
+					break
+				else:
+					continue
+
+			#the projection matrix
+			tempEiVec = tempEiVec[:,0:maxIndex]
+			self._proj_mat = np.real(tempEiVec)
+			print(maxIndex)
+			print(tempEiVal.shape,tempEiVec.shape)
+
+			if verbose:
+				print(mean)
+				print(Z)
+				print(cov)
+				print("eighen values: \n",tempEiVal)
+				print("eighen vectors: \n",tempEiVec)
+				print(tempEiVec.shape)
+
 			return self._proj_mat
 		
 		maxEiIndex = []
@@ -49,12 +89,11 @@ class PCA:
 				continue
 
 		#the projection matrix
-		tempEiVec = tempEiVec[:,0:maxIndex]
-		tempEiVec = np.expand_dims(self._proj_mat,axis=1)
-		self._proj_mat = np.real(tempEiVec)
+		slcEiVec = tempEiVec[:,0:maxIndex]
+		self._proj_mat = np.absolute(slcEiVec)
 
 		with open(load_path,'wb+') as f:
-			np.save(f,self._proj_mat)
+			np.savez(f, name1=tempEiVal, name2=tempEiVec)
 
 		print('computing projected data')
 
@@ -63,10 +102,10 @@ class PCA:
 			print(Z)
 			print(cov)
 			print("eighen values: \n",tempEiVal)
-			print("eighen vectors: \n",tempEiVec)
+			print("eighen vectors: \n",slcEiVec)
 			print(tempEiVec.shape)
 
-		return tempEiVec
+		return slcEiVec
 
 	def project(self, X):
-		return X.dot(self._proj_mat.T)
+		return X.dot(self._proj_mat)
