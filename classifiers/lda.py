7de5e48b7fb7363aa4f452a0d4cb2c392d99dce2
Admin <amrmohamed53@gmail.com>
2018-03-02 15:42:18 +0200

    lda for using with fisherfaces

--- classifiers/lda.py
+++ classifiers/lda.py
@@ -29,7 +29,7 @@ class LDA:
         #within-class matrix
         S = np.zeros((dims, dims))                                         #initializing the matrix
         for cl, mean in zip(range(0, noOfClasses), mean_vectors):
-            class_sc_mat = np.zeros((dims, dims))                          # scatter matrix for every class
+            #class_sc_mat = np.zeros((dims, dims))                         # scatter matrix for every class
 
             mat = X[y == cl+1]
             z = mat - mean
@@ -40,28 +40,28 @@ class LDA:
         overall_mean = np.mean(X, axis=0, keepdims=True)
         centered_means = mean_vectors - overall_mean
         B = np.zeros((dims, dims))
-        for i, mean_vec in enumerate(mean_vectors):  
+        for i, _ in enumerate(mean_vectors):  
             ni = X[y==i+1,:].shape[0] # cardinality of ith class
             print(f'calculating B {i+1}')
             B += ni * (centered_means).T.dot(centered_means)
-        
-
         print('Calculating inverse')
-        S_inv = LA.inv(S)
-        print('Calculating eigenvectors')
+        s_inv = LA.pinv(S)
+        print('Done inverse calculation')
         #calcualting eigenvalues and eigenvectors                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                   
-        eig_vals, eig_vecs = SLA.eigs(S_inv.dot(B), k=r)
+        print('Calculating eigenvectors')
+        eig_vals, eig_vecs = LA.eig(np.dot(s_inv, B))
+        print('Done eigenvectors calculation')
+        print(eig_vals,eig_vecs, sep='\n')
         index = (-eig_vals).argsort()[:r]                #sorting the first r eigenvals 
-        sortedEigenvec = eig_vecs[index]
-        self._proj_mat = np.real(sortedEigenvec)
+        sortedEigenvecs = eig_vecs[:,index]
+        self._proj_mat = np.real(sortedEigenvecs)
         print('computing projected data')
-       
-        
+      
         if verbose:
             print(f'Mean vector class {cl} is {mean_vectors}.')
             print('within-class Scatter Matrix:\n', S)
             print('between-class Scatter Matrix:\n', S)
-            print(f'sorted Eigenvectors\n{sortedEigenvec}')
+            print(f'sorted Eigenvectors\n{sortedEigenvecs}')
             
         with open('./lda_projection.npy','wb+') as f:
             np.save(f,self._proj_mat)
